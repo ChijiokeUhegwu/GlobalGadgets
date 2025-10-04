c686b1089bcf41ffb3b7b4393999df1feb3898fc
ChijiokeUhegwu <101138189+ChijiokeUhegwu@users.noreply.github.com>
2025-10-04 09:45:30 +0100

    create database

--- GlobalGadgets SQLScripts/001-Create-Scripts/000-Create-DB.sql
+++ GlobalGadgets SQLScripts/001-Create-Scripts/000-Create-DB.sql
@@ -1,9 +1,4 @@
 /* ===== 0. Create database and use it ===== */
-IF EXISTS(SELECT * FROM sys.databases WHERE name = 'GlobalGadgetsDB')
-BEGIN
-    ALTER DATABASE GlobalGadgetsDB SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
-    DROP DATABASE GlobalGadgetsDB;
-END
 
 CREATE DATABASE GlobalGadgetsDB;
 GO
