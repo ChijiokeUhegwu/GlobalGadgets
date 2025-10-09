7eeb3295c88195471f0b4e1d057ae46cda5c8c3b
ChijiokeUhegwu <101138189+ChijiokeUhegwu@users.noreply.github.com>
2025-10-09 19:45:48 +0100

    update scripts

--- GlobalGadgets SQLScripts/003-View-Scripts/003.1-Join-Premium-Customers-above-40.sql
+++ GlobalGadgets SQLScripts/003-View-Scripts/003.1-Join-Premium-Customers-above-40.sql
@@ -1,6 +1,13 @@
+USE [GlobalGadgetsDB];
+GO
+
+----------------------------------------------------------------
+--===============| VIEW: vw_OldPremiumCustomers |===============--
+----------------------------------------------------------------
 -- Test Query 1:List all the Customers who are older than 40 and 
 -- have placed an order for a product in the 'Premium' product category. 
 
+CREATE VIEW dbo.vw_OldPremiumCustomers AS
 SELECT DISTINCT 
     c.customer_id,
     c.full_name,
@@ -18,5 +25,19 @@ JOIN dbo.Categories AS cat
 WHERE 
     c.date_of_birth < '1985-01-01'
     AND cat.category_name = 'Premium';
+GO
+
+-- Test the view
+SELECT TOP 1 * FROM dbo.vw_OldPremiumCustomers;
+
+-- Test why it returned an empty dataframe
+SELECT COUNT(*) AS PremiumOrders
+FROM dbo.Orders AS o
+JOIN dbo.OrderDetails AS od ON o.order_id = od.order_id
+JOIN dbo.Products AS p ON od.product_id = p.product_id
+JOIN dbo.Categories AS cat ON p.category_id = cat.category_id
+WHERE cat.category_name = 'Premium';
+
+
 
 
